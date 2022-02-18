43f26d81bff5895d0044c7f875977a9e4f3c0827
TawonaRwatidaR204712J <tnrwatida@gmail.com>
2022-02-18 16:11:51 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+// declaring addition interface
+interface Addition{
+
+    // initializing sum field
+    int sum = 0;
+
+    //declaring add method with no body
+    void add(int num1, int num2);
+}
+
+class Calculator implements Addition{
+
+    // implementing method from interface
+    @Override
+    public void add(int num1, int num2){
+
+        int sum = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + sum);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // instantiating class
+	    Calculator cal = new Calculator();
+        cal.add(10,20);
+
+    }
+}
